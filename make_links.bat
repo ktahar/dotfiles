98e3892eb349f31c593c888d24db5ad63eccb5f3
Kosuke Tahara <ksk.tahara@gmail.com>
2014-12-01 13:10:20 +0900

    Add nv_manual.

--- make_links.bat
+++ make_links.bat
@@ -7,6 +7,7 @@ mklink .unison\nas_nv.prf ..\dotfiles\nas_nv.prf
 mklink .unison\nas_tahara.prf ..\dotfiles\nas_tahara.prf
 mklink .unison\usb.prf ..\dotfiles\usb.prf
 mklink .unison\nv.prf ..\dotfiles\nv.prf
+mklink .unison\nv_manual.prf ..\dotfiles\nv_manual.prf
 mklink vimfiles\skeleton.py ..\dotfiles\.vim\skeleton.py
 mklink /D vimfiles\after ..\dotfiles\.vim\after
 mklink /D vimfiles\bundle.nosync ..\dotfiles\.vim\bundle.nosync
