65b4aac031e5b911e7c4256c5404e1912a3e6b15
Kosuke Tahara <ksk.tahara@gmail.com>
2018-01-24 20:54:31 +0900

    remove unison profiles

--- make_links.bat
+++ make_links.bat
@@ -3,12 +3,6 @@ mklink _vimrc dotfiles\.vimrc
 mklink _gvimrc dotfiles\.gvimrc
 mklink .latexmkrc dotfiles\.latexmkrc
 mklink .matplotlib\matplotlibrc ..\dotfiles\matplotlibrc
-mklink .unison\nas_graphene.prf ..\dotfiles\nas_graphene.prf
-mklink .unison\nas_nv.prf ..\dotfiles\nas_nv.prf
-mklink .unison\nas_tahara.prf ..\dotfiles\nas_tahara.prf
-mklink .unison\usb.prf ..\dotfiles\usb.prf
-mklink .unison\nv.prf ..\dotfiles\nv.prf
-mklink .unison\nv_manual.prf ..\dotfiles\nv_manual.prf
 mklink vimfiles\skeleton.py ..\dotfiles\.vim\skeleton.py
 mklink /D vimfiles\after ..\dotfiles\.vim\after
 echo DONE...
