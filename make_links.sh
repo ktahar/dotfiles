52e69c7ceb3c13dd5b3c2f5fec1645a66da8538d
Kosuke Tahara <ksk.tahara@gmail.com>
2018-02-07 13:22:26 +0900

    add .ctags and .gitignore_global

--- make_links.sh
+++ make_links.sh
@@ -4,6 +4,8 @@ ln -s ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc
 ln -s ${HOME}/dotfiles/.gvimrc ${HOME}/.gvimrc
 ln -s ${HOME}/dotfiles/.latexmkrc ${HOME}/.latexmkrc
 ln -s ${HOME}/dotfiles/.tmux.conf ${HOME}/.tmux.conf
+ln -s ${HOME}/dotfiles/.ctags ${HOME}/.ctags
+ln -s ${HOME}/dotfiles/.gitignore_global ${HOME}/.gitignore_global
 mkdir ${HOME}/.matplotlib
 ln -s ${HOME}/dotfiles/matplotlibrc ${HOME}/.matplotlib/matplotlibrc
 mkdir ${HOME}/.vim
