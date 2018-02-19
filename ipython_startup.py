0dc66675bb8640ca96be756991a5b4c990521140
Kosuke Tahara <ksk.tahara@gmail.com>
2018-02-19 19:11:02 +0900

    add autoreload ipython3

--- ipython_startup.py
+++ ipython_startup.py
@@ -1,5 +1,12 @@
+import sys, os
+
 import numpy as np
 import scipy as sp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
+
+from IPython import get_ipython
+ipython = get_ipython()
+ipython.magic('load_ext autoreload')
+ipython.magic('autoreload 1')
