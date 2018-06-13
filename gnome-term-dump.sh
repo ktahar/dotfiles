70d2a16fbab45ab1a294c2d95b1faa5dcb75ee0b
Kosuke Tahara <ksk.tahara@gmail.com>
2018-06-13 19:46:21 +0900

    add multiple colorscheme func and monokai

--- gnome-term-dump.sh
+++ gnome-term-dump.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-dconf dump /org/gnome/terminal/legacy/profiles:/:$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')/ > gnome-term.dconf
+if [ $# -eq 0 ]; then
+    fn=gnome-term.dconf
+elif [ $# -eq 1 ]; then
+    fn=$1
+else
+    echo "invalid number of arguments."
+    exit 1
+fi
+
+echo "dumping to $fn"
+dconf dump /org/gnome/terminal/legacy/profiles:/:$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')/ > $fn
