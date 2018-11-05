3d118b6f40c9ad88635866a2c7e917c636c93d08
Kosuke Tahara <ksk.tahara@gmail.com>
2018-11-05 18:59:41 +0900

    ipython startup: add json and SUMO

--- ipython_startup.py
+++ ipython_startup.py
@@ -1,5 +1,6 @@
 import sys, os
 import datetime
+import json
 
 import numpy as np
 import scipy as sp
@@ -13,3 +14,7 @@ ipython.magic('load_ext autoreload')
 ipython.magic('autoreload 1')
 
 plt.ion()
+
+if "SUMO_HOME" in os.environ:
+    if os.path.join(os.environ["SUMO_HOME"], "tools") not in sys.path:
+        sys.path.append(os.path.join(os.environ["SUMO_HOME"], "tools"))
