4bcfb1b61052ee12963f17cef9c1592b9ed16ba0
Kosuke Tahara <ksk.tahara@gmail.com>
2018-11-06 12:00:55 +0900

    add gnome-settings

--- desktop/gnome-disable-animations.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.desktop.interface enable-animations false
---
+++ desktop/gnome-settings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface enable-animations false
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
