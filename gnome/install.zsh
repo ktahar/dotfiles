a69ba2e76ff8512f49bf2c36dad9a36f6e44b8ea
Kosuke Tahara <ksk.tahara@gmail.com>
2018-12-19 16:41:38 +0900

    gnome/install.zsh: we don't need this by hand

--- gnome/install.zsh
+++ gnome/install.zsh
@@ -4,11 +4,3 @@
 if [ $(lsb_release -cs) = 'bionic' ]; then
     sudo -E apt install vanilla-gnome-desktop
 fi
-
-# Set launcher icon.
-target=${HOME}/.local/share/applications/idea.desktop
-if [ -e ${HOME}/opt/IntelliJ/idea ] && \
-    [ ! -e $target ]; then
-    sed "s@<HOME>@${HOME}@g" idea.desktop > $target
-    chmod u+x $target
-fi
