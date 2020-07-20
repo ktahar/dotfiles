661e228cb2939eb4afa9a7aacf1b74df76e3eeed
Kosuke Tahara <ksk.tahara@gmail.com>
2020-07-20 23:05:17 +0900

    add git alias f=fetch and import reduce in ipython

--- python/ipython_startup.py
+++ python/ipython_startup.py
@@ -8,6 +8,7 @@ import scipy as sp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
+from functools import reduce
 
 from IPython import get_ipython
 from prompt_toolkit.enums import DEFAULT_BUFFER
@@ -30,9 +31,5 @@ if getattr(ipy, 'pt_app', None):
    registry = ipy.pt_app.key_bindings
    registry.add_binding(Keys.ControlJ, filter=(HasFocus(DEFAULT_BUFFER)
        & ViInsertMode()))(switch_to_navigation_mode)
-
-if "SUMO_HOME" in os.environ:
-    if os.path.join(os.environ["SUMO_HOME"], "tools") not in sys.path:
-        sys.path.append(os.path.join(os.environ["SUMO_HOME"], "tools"))
 
 plt.ion()
