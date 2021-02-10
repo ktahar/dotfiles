b4081a7e3acaa9be0b703b8cbcf7c1d22c12717a
Kosuke Tahara <ksk.tahara@gmail.com>
2021-02-10 15:40:20 +0900

    revert previous and update ipython

--- python/ipython_config.py
+++ python/ipython_config.py
@@ -1,2 +1,3 @@
 ## Shortcut style to use at the prompt. 'vi' or 'emacs'.
-# c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.prompt_includes_vi_mode = True
--- python/ipython_startup.py
+++ python/ipython_startup.py
@@ -11,11 +11,25 @@ import pandas as pd
 from functools import reduce
 
 from IPython import get_ipython
+from prompt_toolkit.enums import DEFAULT_BUFFER
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.filters import HasFocus, ViInsertMode
+from prompt_toolkit.key_binding.vi_state import InputMode
 
 ipy= get_ipython()
 ipy.magic('load_ext autoreload')
 ipy.magic('autoreload 1')
 
 q = exit
+
+# nmap C-J <ESC>
+def switch_to_navigation_mode(event):
+   vi_state = event.cli.vi_state
+   vi_state.input_mode = InputMode.NAVIGATION
+
+if getattr(ipy, 'pt_app', None):
+   registry = ipy.pt_app.key_bindings
+   registry.add_binding(Keys.ControlJ, filter=(HasFocus(DEFAULT_BUFFER)
+       & ViInsertMode()))(switch_to_navigation_mode)
 
 plt.ion()
